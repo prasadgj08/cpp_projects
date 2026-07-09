4afd6ee36464bdd24bc3f33703cca8e8fd8f8cb4
Prasad <prasadgjoshi2008@gmail.com>
2026-07-09 21:40:01 +0530

    updated

--- LeetCodeArr1.cpp
+++ LeetCodeArr1.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    for(int i=0; i<nums.size(); i++) {
+        for(int j=0; j<nums.size(); j++) {
+            if(i!=j && nums[i]+nums[j]==target) {
+                return {i,j};
+            }
+        }
+    }
+        return {};
+}
+
 int main() {
-    cout<<"hello"<<endl;
+    vector<int> nums;
+    int target;
+    twoSum(nums, target);
     return 0;
 }
